0e94d2d7f467b9f57da0598fb64e9ca5dcd974ff
varunkumarnr <varunnrk@gmail.com>
2020-07-29 11:50:34 +0530

    q13

--- p13_largesum.js
+++ p13_largesum.js
@@ -1,6 +1,10 @@
+/* Project Euler: Problem 13: Large sum
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.*/
 function largeSum(arr) {
 
-    return true;
+  let sum = arr.reduce((acc, item) => acc + Number(item), 0);   // 8.348422521139211e+49
+  let str = sum.toString().split('e')[0];   // '8.348422521139211'
+  return 1e+9 * str.slice(0,11)
   }
   
 
@@ -9,5 +13,4 @@ function largeSum(arr) {
     '37107287533902102798797998220837590246510135740250',
     '46376937677490009712648124896970078050417018260538'
   ];
-  
-  largeSum(testNums);
+  console.log(largeSum(testNums));
